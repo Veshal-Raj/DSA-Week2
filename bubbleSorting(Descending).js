91f124c1183eb81648926284c2f4144c232dad7d
Veshal Raj <veshalraj1307@gmail.com>
2023-06-20 15:22:24 +0530

    descending-bubbleSort

--- bubbleSorting(Descending).js
+++ bubbleSorting(Descending).js
@@ -1,9 +1,9 @@
-function bubbleSort(arr){
+function descendingBubbleSort(arr){
     let swapped
     do{
         swapped=false
         for(let i=0;i<arr.length-1;i++){
-            if(arr[i]>arr[i+1]){
+            if(arr[i]<arr[i+1]){
                 let temp=arr[i]
                 arr[i]=arr[i+1]
                 arr[i+1]=temp
@@ -12,6 +12,6 @@ function bubbleSort(arr){
         }
     }while(swapped)
 }
-const arr=[2,11,3,14,5,66]
-bubbleSort(arr)
+const arr=[11,22,2,3,1,44,5,66]
+descendingBubbleSort(arr)
 console.log(arr)
