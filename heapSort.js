9df6a1212b3930d519f7207b9ebcb51ef72db466
Veshal Raj <veshalraj1307@gmail.com>
2023-07-01 15:58:42 +0530

    week 6

---
+++ heapSort.js
@@ -0,0 +1,45 @@
+function heapify(arr, n, i) {
+    let largest = i; // Initialize largest as root
+    let left = 2 * i + 1; // Left child
+    let right = 2 * i + 2; // Right child
+  
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest]) {
+      largest = left;
+    }
+  
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest]) {
+      largest = right;
+    }
+  
+    // If largest is not root
+    if (largest !== i) {
+      [arr[i], arr[largest]] = [arr[largest], arr[i]]; // Swap elements
+      heapify(arr, n, largest); // Recursively heapify the affected sub-tree
+    }
+  }
+  
+  function heapSort(arr) {
+    const n = arr.length;
+  
+    // Build max heap
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+      heapify(arr, n, i);
+    }
+  
+    // Extract elements one by one from the heap
+    for (let i = n - 1; i > 0; i--) {
+      [arr[0], arr[i]] = [arr[i], arr[0]]; // Move current root to the end
+  
+      heapify(arr, i, 0); // Heapify the reduced heap
+    }
+  
+    return arr;
+  }
+  
+  // Example usage:
+  const arr = [12, 11, 13, 5, 6, 7];
+  console.log("Original array:", arr);
+  console.log("Sorted array:", heapSort(arr));
+  
--- inserting&findingValueBST.js
+++ inserting&findingValueBST.js
@@ -1,43 +1,112 @@
-    class TreeNode{
-        constructor(val){
-            this.val=val
-            this.left=null
-            this.right=null
-        }
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BTS {
+  constructor() {
+    this.root = null;
+  }
+
+  Insert(val) {
+    const newNode = new TreeNode(val);
+    if (this.root === null) this.root = newNode;
+    else this.InsertNode(this.root, newNode);
+  }
+
+  InsertNode(node, newNode) {
+    if (newNode.val < node.val) {
+      if (node.left === null) node.left = newNode;
+      else this.InsertNode(node.left, newNode);
+    } else {
+      if (node.right === null) node.right = newNode;
+      else this.InsertNode(node.right, newNode);
     }
-    class BTS{
-        constructor(){
-            this.root=null
-        }
-        Insert(val){
-            const newNode = new TreeNode(val)
-            if(this.root===null)this.root=newNode
-            else this.InsertNode(this.root,newNode)
-        }
-        InsertNode(node, newNode) {
-            if (newNode.val < node.val) {
-              if (node.left === null) node.left = newNode;
-              else this.InsertNode(node.left, newNode);
-            } else {
-              if (node.right === null) node.right = newNode;
-              else this.InsertNode(node.right, newNode);
-            }
-          }
-          
-        print(){
-            this.printnode(this.root)
-        }
-        
-        printnode(node) {
-            if (node === null) return;
-            this.printnode(node.left);
-            console.log(node.val);
-            this.printnode(node.right);
-          }
-          
+  }
+
+  search(val) {
+    return this.searchNode(this.root, val);
+  }
+
+  searchNode(node, val) {
+    if (node === null || node.val === val) return node;
+    if (val < node.val) return this.searchNode(node.left, val);
+    else return this.searchNode(node.right, val);
+  }
+   findClosestValue(val){
+    if(this.search(val))return val
+    let prev = val-1
+    let post = val+1
+    while(true){
+      if(this.search(prev)) return prev
+      else prev--
+      if(this.search(post))return post
+      else post++
     }
-    const bts = new BTS()
-    bts.Insert(1)
-    bts.Insert(2)
-    bts.Insert(3)
-    bts.print()
+   }
+
+  delete(val) {
+    this.root = this.deleteNode(this.root, val);
+  }
+
+  deleteNode(node, val) {
+    if (node === null) return null;
+    if (val < node.val) node.left = this.deleteNode(node.left, val);
+    else if (val > node.val) node.right = this.deleteNode(node.right, val);
+    else {
+      if (node.left === null && node.right === null) return null;
+      else if (node.left === null) return node.right;
+      else if (node.right === null) return node.left;
+      else {
+        const minValue = this.findMinValue(node.right);
+        node.val = minValue;
+        node.right = this.deleteNode(node.right, minValue);
+      }
+      return node;
+    }
+  }
+
+  findMinValue(node) {
+    let curr = node;
+    while (curr.left) {
+      curr = curr.left;
+    }
+    return curr.val;
+  }
+  min(root){
+    if(!root.left) return root.val
+    else return this.min(root.left)
+  }
+
+  max(root){
+    if(!root.right) return root.val
+    else return this.max(root.right)
+  }
+
+  print() {
+    this.printnode(this.root);
+  }
+
+  printnode(node) {
+    if (node === null) return;
+    console.log(node.val);
+    if (node.left) this.printnode(node.left);
+    if (node.right) this.printnode(node.right);
+  }
+}
+
+
+const bts = new BTS();
+bts.Insert(10);
+bts.Insert(2);
+bts.Insert(33);
+bts.Insert(4);
+bts.Insert(55);
+bts.Insert(6);
+bts.print();
+console.log(bts.findClosestValue(30))
+console.log("max: "+bts.min(bts.root))
+console.log("min: "+bts.max(bts.root))
